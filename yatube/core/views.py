0958570bb23d83cdd362a7325912f6973062f899
Сергей Мартышов <martyshovss@yandex.ru>
2022-08-31 22:13:18 +0400

    Debug

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -4,16 +4,27 @@ from django.shortcuts import render
 
 
 def page_not_found(request, exception):
-    return render(request, 'core/404.html', status=HTTPStatus.NOT_FOUND)
+    return render(
+        request,
+        'core/404.html',
+        status=HTTPStatus.NOT_FOUND
+    )
 
 
 def server_error(request):
-    return render(request, 'core/500.html',
-                  status=HTTPStatus.INTERNAL_SERVER_ERROR)
+    return render(
+        request,
+        'core/500.html',
+        status=HTTPStatus.INTERNAL_SERVER_ERROR
+    )
 
 
 def permission_denied(request, exception):
-    return render(request, 'core/403.html', status=HTTPStatus.FORBIDDEN)
+    return render(
+        request,
+        'core/403.html',
+        status=HTTPStatus.FORBIDDEN
+    )
 
 
 def csrf_failure(request, reason=''):
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -16,6 +15,6 @@ handler500 = 'core.views.server_error'
 handler403 = 'core.views.permission_denied'
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+    import debug_toolbar
+
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
